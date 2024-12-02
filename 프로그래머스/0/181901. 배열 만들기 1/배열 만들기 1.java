cb1a25ff9ca5c1d13c437e8987e487e0c56d209f
김재환 <ma2582m@naver.com>
2024-12-03 02:30:00 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.07 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,23 +1,14 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] solution(int n, int k) {
-        // 동적으로 값을 저장하기 위해 ArrayList 사용
-        ArrayList<Integer> multiples = new ArrayList<>();
-        
-        // 1 이상 n 이하의 정수 중 k의 배수 찾기
-        for (int i = 1; i <= n; i++) {
-            if (i % k == 0) {
-                multiples.add(i);
-            }
+        int count = n / k;
+
+
+        int[] answer = new int[count];
+
+        for (int i = 1; i <= count; i++) {
+            answer[i - 1] = k * i;
         }
-        
-        // ArrayList를 배열로 변환
-        int[] answer = new int[multiples.size()];
-        for (int i = 0; i < multiples.size(); i++) {
-            answer[i] = multiples.get(i);
-        }
-        
+
         return answer;
     }
 }
