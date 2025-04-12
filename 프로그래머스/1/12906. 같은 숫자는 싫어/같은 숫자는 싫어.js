53a71c0a6e9096cb39b662da589db12a9bcf96d0
김재환 <ma2582m@naver.com>
2025-04-12 23:31:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.87 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,10 +1,4 @@
 function solution(arr)
 {
-    let result = [];
-    
-    arr.forEach((val,idx) => {
-        if(arr[idx] !== arr[idx+1]) result.push(val);
-    });
-    
-    return result
+    return arr.filter((val,index) => val != arr[index+1]);
 }
