4c2a7b8d8a2545479181ae1b26df07c845892ad7
김재환 <ma2582m@naver.com>
2025-04-17 23:56:51 +0900

    [level 2] Title: 의상, Time: 0.31 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,14 +1,9 @@
+// 종류별로 최대 1가지 의상만 착용 가능, 하루에 최소 한 개의 의상 착용
+// 종류가 다르면 여러 의상 착용 가능
+
 function solution(clothes) {
-    let count = new Map();
-    let object = [];
-    
-    for([,type] of clothes){
-        count.set(type, count.has(type)?count.get(type)+1:1);
-    }
-    
-    for([,v] of count){
-        object.push(v+1);
-    }
-    
-    return object.reduce((t,e)=>t * e, 1) - 1;
+    return Object.values(clothes.reduce((obj, t)=> {
+        obj[t[1]] = obj[t[1]] ? obj[t[1]] + 1 : 1;
+        return obj;
+    } , {})).reduce((a,b)=> a*(b+1), 1)-1;    
 }
