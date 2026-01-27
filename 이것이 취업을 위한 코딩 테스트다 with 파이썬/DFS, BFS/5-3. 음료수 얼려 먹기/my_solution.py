33ecb5739096cce34219900d621921a8d450da72
KimJaehwan <ma2582m@naver.com>
2026-01-28 01:10:35 +0900

    DFS-BFS: 5-3 음료수 얼려 먹기

---
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/DFS, BFS/5-3. 음료수 얼려 먹기/my_solution.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+maps = []
+count = 0
+for _ in range(n):
+    maps.append(list(map(int, input())))
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if maps[x][y] == 0:
+        maps[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x + 1, y)
+        dfs(x, y - 1)
+        dfs(x, y + 1)
+        return True
+    return False
+
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            count += 1
+
+print(count)
