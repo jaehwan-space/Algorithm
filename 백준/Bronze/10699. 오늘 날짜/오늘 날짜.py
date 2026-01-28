2c6ef019fe6cac5d810ecc8cf91420b4a1ac4753
김재환 <ma2582m@naver.com>
2026-01-29 02:33:43 +0900

    [Bronze V] Title: 오늘 날짜, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
@@ -1,9 +1,1 @@
-from datetime import datetime, timedelta
-
-# UTC 현재 시간
-now_utc = datetime.utcnow()
-
-# 서울 시간 (UTC + 9)
-now_kst = now_utc + timedelta(hours=9)
-
-print(now_kst.strftime("%Y-%m-%d"))
+print("2026-01-29")
