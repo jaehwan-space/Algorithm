6584fbdeaf8df58025bbaf3e0a626f7e64361091
KimJaehwan <ma2582m@naver.com>
2026-02-01 00:05:33 +0900

    Sort: 6-2 위에서 아래로

---
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/정렬/6-2. 위에서 아래로/my_solution.py
@@ -0,0 +1,14 @@
+n = int(input())
+array = []
+
+for _ in range(n):
+    array.append(int(input()))
+
+for i in range(len(array)):
+    min_index = i
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[min_index], array[i] = array[i], array[min_index]
+
+print(*array)
