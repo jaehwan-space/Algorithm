1c55659e82c6a6f9997c8f521719e401d74135dc
Jaehwan Kim <ma2582m@naver.com>
2026-07-10 10:54:55 +0900

    [level 0] Title: 배열 만들기 4, Time: 21.33 ms, Memory: 15.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    i = 0
+    stk = []
+    while (i < len(arr)):
+        if len(stk) == 0:
+            stk.append(arr[i])
+            i += 1
+        else:
+            if stk[-1] < arr[i]:
+                stk.append(arr[i])
+                i += 1
+            else:
+                stk.pop()
+    return stk
