a8fa570154933939bec5e261f1dc98b4e446a3c6
Jaehwan Kim <ma2582m@naver.com>
2026-07-17 10:31:33 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,7 @@
+def solution(str_list):
+    for i, k in enumerate(str_list):
+        if k == "l":
+            return str_list[:i]
+        elif k =="r":
+            return str_list[i+1:]
+    return []
