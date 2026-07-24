09913b5e28adc438d1b34f7b0e98a0861716b54d
Jaehwan Kim <ma2582m@naver.com>
2026-07-24 09:30:58 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = 0
+    for i in num_list:
+        while not i == 1:
+            if i % 2 == 0:
+                i //= 2
+            else:
+                i = (i - 1) // 2
+            answer += 1
+            
+    return answer
